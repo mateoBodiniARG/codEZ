12da6034564769be3e6f44ea18522ff108588aea
Mateo Bodini <bodinidev@gmail.com>
2023-12-21 23:05:29 -0300

    cambios en inicio

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -5,7 +5,7 @@ import { PiCaretDoubleDown } from "react-icons/pi";
 import { recursosObj } from "../recursosObj";
 import "./Inicio.css";
 import Navbar from "../Navbar/Navbar";
-
+import { Fade } from "react-awesome-reveal";
 const Inicio = () => {
   return (
     <div className="text-white bg-gray-950 bg-gradient-personal">
@@ -14,9 +14,11 @@ const Inicio = () => {
       <div className="relative overflow-hidden h-screen ">
         <div className="flex items-center justify-center h-full flex-col">
           <div className="text-center">
-            <h1 className="text-6xl font-extrabold relative mb-4 xs:m-2 xs:text-3xl">
-              ¡Bienvenido a codEZ!
-            </h1>
+            <Fade direction="down">
+              <h1 className="text-6xl font-extrabold relative mb-4 xs:m-2 xs:text-3xl">
+                ¡Bienvenido a codEZ!
+              </h1>
+            </Fade>
             <p className="text-gray-500 max-w-lg mx-auto mb-6 xs:mx-4 xs:text-xs xs:font-bold font-medium">
               Recursos para simplificar el desarrollo en diseño y programación,
               haciendo que aprender y crear sean Accesibles y Emocionantes.
